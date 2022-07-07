08661fd5053fa1e24edc8b65cb5963921bae67c8
rejuvyesh <mail@rejuvyesh.com>
2022-07-07 21:24:38 +0000

    shuffle&shard at file level, still have an extra dimension when using dataloader

--- tests/test_datapipe.py
+++ tests/test_datapipe.py
@@ -3,6 +3,7 @@ from tkinter import Y
 import torch
 import torchdata.datapipes as dp
 from src.datamodules.era5_datapipe import ERA5Forecast, ERA5Npy, IndividualDataIter
+from torch.utils.data import DataLoader
 
 NPY_PATH = "/mnt/data/1.40625/_yearly_np"
 
@@ -17,18 +18,22 @@ batchsize = 32
 dp = (
     IndividualDataIter(
         ERA5Forecast(
-            ERA5Npy(dp.iter.FileLister(NPY_PATH), variables=["t", "u10", "v10"],)
-            .shuffle(buffer_size=4)
-            .sharding_filter()
+            ERA5Npy(
+                dp.iter.FileLister(NPY_PATH)
+                .shuffle(buffer_size=4)  # shuffle at the year level
+                .sharding_filter(),  # needed for num_workers > 1
+                variables=["t", "u10", "v10"],
+            )
         ),
     )
-    .shuffle(buffer_size=1000)
+    .shuffle(buffer_size=1000)  # shuffle at the individual data level
     .batch(batchsize)
-    .in_batch_shuffle()
+    .in_batch_shuffle()  # shuffle within a batch, probably not necessary
     .collate(collate_fn)
 )
 
-for x, y in dp:
+
+for x, y in DataLoader(dp):
     print(x.shape, y.shape)
     import pdb
 
