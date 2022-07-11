9fa0a4f7e98c55e0713c30b69626b78846647d09
tung-nd <ductungnguyen1997@gmail.com>
2022-07-11 17:12:40 +0000

    black autoformat

--- src/models/vit_module.py
+++ src/models/vit_module.py
@@ -1,5 +1,5 @@
 # credits: https://github.com/ashleve/lightning-hydra-template/blob/main/src/models/mnist_module.py
-from typing import Any, List
+from typing import Any
 
 import torch
 from pytorch_lightning import LightningModule
@@ -11,7 +11,7 @@ class ViTLitModule(LightningModule):
         net: torch.nn.Module,
         pretrained_path: str,
         lr: float = 0.001,
-        weight_decay: float = 0.005
+        weight_decay: float = 0.005,
     ):
         super().__init__()
         self.save_hyperparameters(logger=False, ignore=["net"])
@@ -23,11 +23,14 @@ class ViTLitModule(LightningModule):
         checkpoint = torch.load(pretrained_path)
 
         print("Loading pre-trained checkpoint from: %s" % pretrained_path)
-        checkpoint_model = checkpoint['state_dict']
+        checkpoint_model = checkpoint["state_dict"]
         state_dict = self.state_dict()
         checkpoint_keys = list(checkpoint_model.keys())
         for k in checkpoint_keys:
-            if k not in state_dict.keys() or checkpoint_model[k].shape != state_dict[k].shape:
+            if (
+                k not in state_dict.keys()
+                or checkpoint_model[k].shape != state_dict[k].shape
+            ):
                 print(f"Removing key {k} from pretrained checkpoint")
                 del checkpoint_model[k]
 
@@ -42,14 +45,26 @@ class ViTLitModule(LightningModule):
         x, y = batch
         loss_dict, _ = self.net.forward(x, y)
         for var in loss_dict.keys():
-            self.log("train/" + var, loss_dict[var], on_step=True, on_epoch=False, prog_bar=True)
+            self.log(
+                "train/" + var,
+                loss_dict[var],
+                on_step=True,
+                on_epoch=False,
+                prog_bar=True,
+            )
         return loss_dict
 
     def validation_step(self, batch: Any, batch_idx: int):
         x, y = batch
         loss_dict, _ = self.net.forward(x, y)
         for var in loss_dict.keys():
-            self.log("val/" + var, loss_dict[var], on_step=False, on_epoch=True, prog_bar=False)
+            self.log(
+                "val/" + var,
+                loss_dict[var],
+                on_step=False,
+                on_epoch=True,
+                prog_bar=False,
+            )
         return loss_dict
 
     # def validation_epoch_end(self, outputs: List[Any]):
