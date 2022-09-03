fix time-only masking

src/models/components/tokenized_video_mae.py
@@ -202,7 +202,7 @@ class TokenizedVideoMAE(TokenizedBase):
     def time_masking(self, x, mask_ratio):
         # x: B, T, L, D
         N, T, L, D = x.shape
-        len_keep = L - int(T * mask_ratio)
+        len_keep = T - int(T * mask_ratio)
 
         noise = torch.rand(N, T, device=x.device)
 
@@ -359,11 +359,11 @@ class TokenizedVideoMAE(TokenizedBase):
 # loss, pred, mask = model(x)
 # print (loss)
 
-x = torch.randn(2, 2, 3, 2)
-mask_ratio = 0.5
+# x = torch.randn(2, 2, 3, 2)
+# mask_ratio = 0.5
 
-N, T, L, D = x.shape
-len_keep = L - int(L * mask_ratio)
+# N, T, L, D = x.shape
+# len_keep = L - int(L * mask_ratio)
 
 # noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
 
@@ -379,24 +379,26 @@ len_keep = L - int(L * mask_ratio)
 # # unshuffle to get the binary mask
 # mask = torch.gather(mask, dim=1, index=ids_restore)
 
-noise = torch.rand(N, T, device=x.device)
+# len_keep = T - int(T * mask_ratio)
 
-ids_shuffle = torch.argsort(noise, dim=1) # N, T
-ids_restore = torch.argsort(ids_shuffle, dim=1) # N, T
+# noise = torch.rand(N, T, device=x.device)
 
-# # keep the first subset
-ids_keep = ids_shuffle[:, :len_keep] # N, T*(1-mask_ratio)
-x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, L, D)) # B, T*(1-mask_ratio), L, D
+# ids_shuffle = torch.argsort(noise, dim=1) # N, T
+# ids_restore = torch.argsort(ids_shuffle, dim=1) # N, T
 
-mask = torch.ones([N, T], device=x.device)
-mask[:, :len_keep] = 0
-# unshuffle to get the binary mask
-mask = torch.gather(mask, dim=1, index=ids_restore)
+# # # keep the first subset
+# ids_keep = ids_shuffle[:, :len_keep] # N, T*(1-mask_ratio)
+# x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, L, D)) # B, T*(1-mask_ratio), L, D
 
-for i in range(N):
-    print ('x', x[i])
-    print ('ids shuffle', ids_shuffle[i])
-    print ('id restore', ids_restore[i])
-    print ('ids keep', ids_keep[i])
-    print ('x masked', x_masked[i])
-    print ('=' * 50)
+# mask = torch.ones([N, T], device=x.device)
+# mask[:, :len_keep] = 0
+# # unshuffle to get the binary mask
+# mask = torch.gather(mask, dim=1, index=ids_restore)
+
+# for i in range(N):
+#     print ('x', x[i])
+#     print ('ids shuffle', ids_shuffle[i])
+#     print ('id restore', ids_restore[i])
+#     print ('ids keep', ids_keep[i])
+#     print ('x masked', x_masked[i])
+#     print ('=' * 50)