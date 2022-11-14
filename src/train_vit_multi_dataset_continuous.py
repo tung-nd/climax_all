10ea026cf27c23a94d33f583df9d13c59d0b7613
tung-nd <ductungnguyen1997@gmail.com>
2022-11-14 06:50:00 +0000

    fixing deepspeed

--- src/train_vit_multi_dataset_continuous.py
+++ src/train_vit_multi_dataset_continuous.py
@@ -1,5 +1,7 @@
 import os
 
+from pytorch_lightning import Trainer
+from pytorch_lightning.strategies.deepspeed import DeepSpeedStrategy
 from pytorch_lightning.utilities.cli import LightningCLI
 
 from models.vit_continuous_module import ViTContinuousLitModule
@@ -18,7 +20,38 @@ def main():
         auto_registry=True,
         parser_kwargs={"parser_mode": "omegaconf", "error_handler": None},
     )
-    os.makedirs(cli.trainer.default_root_dir, exist_ok=True)
+
+    # trainer = Trainer(
+    #     default_root_dir=cli.trainer.default_root_dir,
+    #     precision=cli.trainer.precision,
+    #     gpus=cli.trainer.gpus,
+    #     devices=cli.trainer.devices,
+    #     num_nodes=cli.trainer.num_nodes,
+    #     accelerator=cli.trainer.accelerator,
+    #     min_epochs=cli.trainer.min_epochs,
+    #     max_epochs=cli.trainer.max_epochs,
+    #     enable_progress_bar=True,
+    #     sync_batchnorm=True,
+    #     enable_checkpointing=True,
+    #     resume_from_checkpoint=None,
+    #     limit_val_batches=cli.trainer.limit_val_batches,
+    #     num_sanity_val_steps=cli.trainer.num_sanity_val_steps,
+    #     fast_dev_run=cli.trainer.fast_dev_run,
+    #     logger=cli.trainer.logger,
+    #     callbacks=cli.trainer.callbacks,
+    #     strategy=DeepSpeedStrategy(
+    #         stage=1,
+    #         # loss_scale=2**16,
+    #         # min_loss_scale=2**10,
+    #         # loss_scale_window=500,
+    #         initial_scale_power=10,
+    #         logging_batch_size_per_gpu=cli.datamodule.hparams.batch_size
+    #     )
+    # )
+
+    trainer = cli.trainer
+
+    os.makedirs(trainer.default_root_dir, exist_ok=True)
 
     # normalization = cli.datamodule.output_transforms
     # mean_norm, std_norm = normalization.mean, normalization.std
@@ -28,7 +61,7 @@ def main():
     # cli.model.set_pred_range(cli.datamodule.hparams.dict_predict_ranges)
 
     # fit() runs the training
-    cli.trainer.fit(cli.model, datamodule=cli.datamodule)
+    trainer.fit(cli.model, datamodule=cli.datamodule)
 
 
 if __name__ == "__main__":
